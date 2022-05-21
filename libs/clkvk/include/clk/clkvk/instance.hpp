5e4cab16848396be7289cb299d6b138fb41c897a
Alex Todoran <alex-tdrn@protonmail.com>
2022-05-21 15:26:24 +0200

    instance creation working

--- libs/clkvk/include/clk/clkvk/instance.hpp
+++ libs/clkvk/include/clk/clkvk/instance.hpp
@@ -5,6 +5,7 @@
 
 #include <clk/base/algorithm.hpp>
 #include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_handles.hpp>
 #include <vulkan/vulkan_structs.hpp>
 
 namespace clk::clkvk
@@ -25,12 +26,14 @@ private:
 	void update() override;
 
 	clk::input_of<vk::ApplicationInfo> _application_info{"Application Info"};
+	clk::input_of<std::vector<std::string>> _extensions{"Extensions"};
 	clk::output_of<vk::UniqueInstance> _instance{"Instance"};
 };
 
 inline instance::instance()
 {
 	register_port(_application_info);
+	register_port(_extensions);
 
 	register_port(_instance);
 }
@@ -39,6 +42,16 @@ inline void instance::update()
 {
 	vk::InstanceCreateInfo instance_info;
 	instance_info.setPApplicationInfo(&(_application_info.data()));
+
+	std::vector<const char*> extensions_proxy;
+	extensions_proxy.reserve(_extensions->size());
+	for(const auto& extension : *_extensions)
+	{
+		extensions_proxy.push_back(extension.c_str());
+	}
+	instance_info.setPEnabledExtensionNames(extensions_proxy);
+
+	*_instance = vk::createInstanceUnique(instance_info);
 }
 
 } // namespace clk::clkvk
