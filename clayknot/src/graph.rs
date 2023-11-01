cc21905651775c135966654cf603f8614ba49630
alex-tdrn <alex-tdrn@protonmail.com>
2023-11-01 19:53:02 +0100

    wippp

--- clayknot/src/graph.rs
+++ clayknot/src/graph.rs
@@ -7,10 +7,10 @@ pub use crate::output::*;
 pub struct NodeId(u64);
 
 #[derive(Debug, Copy, Clone, Hash, Default, PartialEq, Eq)]
-struct InputId(u64);
+pub struct InputId(u64);
 
 #[derive(Debug, Copy, Clone, Hash, Default, PartialEq, Eq)]
-struct OutputId(u64);
+pub struct OutputId(u64);
 
 struct Node {
     inputs: Vec<InputId>,
@@ -22,9 +22,65 @@ pub struct Graph {
     inputs: HashMap<InputId, Box<dyn Input>>,
     outputs: HashMap<OutputId, Box<dyn Output>>,
     nodes: HashMap<NodeId, Node>,
+    next_node_id: u64,
+    next_input_id: u64,
+    next_output_id: u64,
 }
 
 impl Graph {
+    fn get_next_node_id(&mut self) -> NodeId {
+        let node_id = NodeId(self.next_node_id);
+        self.next_node_id += 1;
+        node_id
+    }
+
+    fn get_next_input_id(&mut self) -> InputId {
+        let input_id = InputId(self.next_input_id);
+        self.next_input_id += 1;
+        input_id
+    }
+
+    fn get_next_output_id(&mut self) -> OutputId {
+        let output_id = OutputId(self.next_output_id);
+        self.next_output_id += 1;
+        output_id
+    }
+
+    pub fn add_node(
+        &mut self,
+        inputs: Vec<Box<dyn Input>>,
+        outputs: Vec<Box<dyn Output>>,
+        function: Box<dyn Fn(Vec<&dyn Input>, Vec<&mut dyn Output>)>,
+    ) -> (NodeId, Vec<InputId>, Vec<OutputId>) {
+        let mut input_ids = Vec::new();
+        let mut output_ids = Vec::new();
+
+        for input in inputs {
+            let input_id = self.get_next_input_id();
+            input_ids.push(input_id);
+            self.inputs.insert(input_id, input);
+        }
+
+        for output in outputs {
+            let output_id = self.get_next_output_id();
+            output_ids.push(output_id);
+            self.outputs.insert(output_id, output);
+        }
+
+        let node_id = self.get_next_node_id();
+
+        self.nodes.insert(
+            node_id,
+            Node {
+                inputs: input_ids.clone(),
+                outputs: output_ids.clone(),
+                function,
+            },
+        );
+
+        (node_id, input_ids, output_ids)
+    }
+
     pub fn run_node(&mut self, node: &NodeId) {
         let mut inputs: Vec<&dyn Input> = Vec::new();
         let mut outputs: Vec<&mut dyn Output> = Vec::new();
